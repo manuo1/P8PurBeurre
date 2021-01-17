5a0f1d74c510251a2f0dfb2cba977265d8b5d198
Emmanuel OUDOT <manuo1@free.fr>
2021-01-17 14:09:21 +0100

    adding deployment elements change ALLOWED_HOSTS

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get("ENV", "development") == "production" else True
 
-ALLOWED_HOSTS = [".herokuapps.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
