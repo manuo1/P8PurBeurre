cd13a3c0b4052a447ded29f1b105b6d6362d86dd
Emmanuel OUDOT <manuo1@free.fr>
2021-01-18 14:41:55 +0100

    adding deployment elements change try again

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -154,3 +154,19 @@ LOGIN_REDIRECT_URL = 'profilePage'
 django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
