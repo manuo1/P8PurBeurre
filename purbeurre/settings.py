12b8d357722823ce9672430a617e84f8df91c0d1
Emmanuel OUDOT <manuo1@free.fr>
2021-01-18 14:29:18 +0100

    adding deployment elements change try again

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False if os.environ.get("ENV", "development") == "production" else True
-DEBUG = True
+DEBUG = False if os.environ.get("ENV", "development") == "production" else True
+#DEBUG = True
 
 ALLOWED_HOSTS = ["purbeurre-mo1.herokuapp.com", "localhost", "127.0.0.1"]
-#ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
